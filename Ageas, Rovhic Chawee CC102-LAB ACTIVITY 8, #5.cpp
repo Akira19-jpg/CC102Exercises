0adc08736dd0e8bc7289b0b1a78062588c9071fe
Akira <ty202502697@wmsu.edu.ph>
2026-04-26 14:03:33 +0800

    Add files via upload

---
+++ Ageas, Rovhic Chawee CC102-LAB ACTIVITY 8, #5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+using namespace std;
+
+int main() {
+    ifstream inFile("WORDS.TXT");
+    ofstream outFile("OUTPUT.TXT");
+    set<string> words;
+    string word;
+
+    if (!inFile || !outFile) {
+        cout << "Error opening file\n";
+        return 1;
+    }
+
+    while (inFile >> word) {
+        words.insert(word);
+    }
+
+    for (const auto& w : words) {
+        outFile << w << " ";
+    }
+
+    inFile.close();
+    outFile.close();
+
+    cout << "Duplicates removed. Check OUTPUT.TXT\n";
+    return 0;
+}
